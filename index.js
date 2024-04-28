f72a15ad62761a718ec13cfdf9baf89e9cd49247
Aman2104 <amanyara88@gmail.com>
2024-04-28 17:42:27 +0530

    init

---
+++ index.js
@@ -0,0 +1,167 @@
+require("dotenv").config();
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const { storage } = require("./cloudConfig.js")
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI);
+
+app.get("/", (req, res) => {
+  res.send("Hello Pushkar")
+})
+
+const upload = multer({ storage: storage })
+
+app.post("/upload", upload.single('product'), (req, res) => {
+  res.send(req.file)
+})
+
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number
+  },
+  old_price: {
+    type: Number
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  avilable: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+
+app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  }
+  else { id = 1; }
+  const product = new Product({
+    id: id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+  });
+  await product.save();
+  console.log("Saved");
+  res.json({ success: true, name: req.body.name })
+});
+
+app.post("/removeproduct", async (req, res) => {
+  const product = await Product.findOneAndDelete({ id: req.body.id });
+  console.log("Removed");
+  res.json({ success: true, name: req.body.name })
+});
+
+
+app.get("/allproducts", async (req, res) => {
+  let products = await Product.find({});
+  console.log("All Products Fetched");
+  res.send(products);
+});
+
+const Users = mongoose.model("Users", {
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  cartData: {
+    type: Object,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+app.post('/signup', async (req, res) => {
+  let success = false;
+  let check = await Users.findOne({ email: req.body.email });
+  if (check) {
+    return res.status(400).json({ success: success, errors: "existing user found with this email" });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  });
+  await user.save();
+  const data = {
+    user: {
+      id: user.id
+    }
+  }
+
+  const token = jwt.sign(data, 'secret_ecom');
+  res.json({ success: true, token })
+})
+
+app.post('/login', async (req, res) => {
+  let success = false;
+  let user = await Users.findOne({ email: req.body.email });
+  if (user) {
+    const passCompare = req.body.password === user.password;
+    if (passCompare) {
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      const token = jwt.sign(data, 'secret_ecom');
+      res.json({ success: true, token });
+    }
+    else {
+      return res.status(400).json({ success: success, errors: "Password is Incorrect" })
+    }
+  }
+  else {
+    return res.status(400).json({ success: success, errors: "Wrong Email Id" })
+  }
+})
+
+app.listen(port, (error) => {
+  console.log("Server Running on port " + port);
+});
